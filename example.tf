ce901df984b8e817725b2a341ce25fd84bd1e792
Oliver Frolovs <olliefr@users.noreply.github.com>
2020-08-27 08:53:44 +0100

    Initial commit

---
+++ example.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-04edc9c2bfcf9a772"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
