bf753832a519b0649f8d58d93aa643afe3f94fc7
Oliver Frolovs <olliefr@users.noreply.github.com>
2020-08-31 09:41:44 +0100

    Change DynamoDB billing to "on demand"

--- example.tf
+++ example.tf
@@ -45,6 +45,7 @@ resource "random_pet" "table_name" {}
 resource "aws_dynamodb_table" "tfc_example_table" {
   name = "${var.db_table_name}-${random_pet.table_name.id}"
 
+  billing_mode = "PAY_PER_REQUEST"
   read_capacity  = var.db_read_capacity
   write_capacity = var.db_write_capacity
   hash_key       = "UUID"
